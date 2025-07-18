ef3c5568bbcc8fc072919717fd49437c6d7814c9
Kenan <48903906+Bielteknik@users.noreply.github.com>
2025-07-18 16:39:02 +0300

    new design model.py

--- web/dashboard/models.py
+++ web/dashboard/models.py
@@ -1,68 +1,71 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
+# web/dashboard/models.py (Temizlenmiş ve Düzenlenmiş Hali)
+
 from django.db import models
 
+# Django'nun bu tabloları yönetmesini istemediğimizi belirtmek için
+# "managed = False" satırını koruyoruz. Bu çok önemli!
+# Bu, Django'nun 'migrate' komutuyla bu tablolara dokunmasını engeller.
 
-class AnomalyLogs(models.Model):
-    timestamp = models.DateTimeField(blank=True, null=True)
-    sensor = models.CharField()
-    anomaly_type = models.CharField()
-    value = models.CharField(blank=True, null=True)
-    details = models.CharField(blank=True, null=True)
+class Reading(models.Model):
+    id = models.AutoField(primary_key=True)
+    timestamp = models.DateTimeField()
+    distance_mm = models.FloatField(blank=True, null=True)
+    snow_weight_kg = models.FloatField(blank=True, null=True)
+    snow_height_mm = models.FloatField(blank=True, null=True)
+    snow_density_kg_m3 = models.FloatField(blank=True, null=True)
+    swe_mm = models.FloatField(blank=True, null=True)
+    temperature_c = models.FloatField(blank=True, null=True)
+    humidity_percent = models.FloatField(blank=True, null=True)
+    data_source = models.TextField()
 
     class Meta:
         managed = False
-        db_table = 'anomaly_logs'
-
+        db_table = 'readings'
+        ordering = ['-timestamp'] # Kayıtları en yeniden en eskiye doğru sırala
 
 class ApiQueue(models.Model):
-    timestamp = models.DateTimeField(blank=True, null=True)
-    payload = models.CharField()
-    attempts = models.IntegerField(blank=True, null=True)
+    id = models.AutoField(primary_key=True)
+    timestamp = models.DateTimeField()
+    payload = models.TextField()
+    attempts = models.IntegerField()
 
     class Meta:
         managed = False
         db_table = 'api_queue'
 
+class AnomalyLog(models.Model):
+    id = models.AutoField(primary_key=True)
+    timestamp = models.DateTimeField()
+    sensor = models.TextField()
+    anomaly_type = models.TextField()
+    value = models.TextField(blank=True, null=True)
+    details = models.TextField(blank=True, null=True)
 
-class EmailLogs(models.Model):
-    timestamp = models.DateTimeField(blank=True, null=True)
-    recipient = models.CharField()
-    subject = models.CharField()
+    class Meta:
+        managed = False
+        db_table = 'anomaly_logs'
+        ordering = ['-timestamp']
+
+class EmailLog(models.Model):
+    id = models.AutoField(primary_key=True)
+    timestamp = models.DateTimeField()
+    recipient = models.TextField()
+    subject = models.TextField()
 
     class Meta:
         managed = False
         db_table = 'email_logs'
+        ordering = ['-timestamp']
 
-
-class Readings(models.Model):
-    timestamp = models.DateTimeField(blank=True, null=True)
-    distance_mm = models.TextField(blank=True, null=True)  # This field type is a guess.
-    snow_weight_kg = models.TextField(blank=True, null=True)  # This field type is a guess.
-    snow_height_mm = models.TextField(blank=True, null=True)  # This field type is a guess.
-    snow_density_kg_m3 = models.TextField(blank=True, null=True)  # This field type is a guess.
-    swe_mm = models.TextField(blank=True, null=True)  # This field type is a guess.
-    temperature_c = models.TextField(blank=True, null=True)  # This field type is a guess.
-    humidity_percent = models.TextField(blank=True, null=True)  # This field type is a guess.
-    data_source = models.CharField(blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'readings'
-
-
-class SystemHealthLogs(models.Model):
-    timestamp = models.DateTimeField(blank=True, null=True)
-    cpu_temp_c = models.TextField(blank=True, null=True)  # This field type is a guess.
-    cpu_usage_percent = models.TextField(blank=True, null=True)  # This field type is a guess.
-    memory_usage_percent = models.TextField(blank=True, null=True)  # This field type is a guess.
-    disk_usage_percent = models.TextField(blank=True, null=True)  # This field type is a guess.
+class SystemHealthLog(models.Model):
+    id = models.AutoField(primary_key=True)
+    timestamp = models.DateTimeField()
+    cpu_temp_c = models.FloatField(blank=True, null=True)
+    cpu_usage_percent = models.FloatField(blank=True, null=True)
+    memory_usage_percent = models.FloatField(blank=True, null=True)
+    disk_usage_percent = models.FloatField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'system_health_logs'
+        ordering = ['-timestamp']
